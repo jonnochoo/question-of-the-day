1f1dc53a5fc54a933c5cd1fd9c3577b4714f1bef
Jonathon Choo <jonno.choo@gmail.com>
2015-12-16 21:40:38 +1100

    Make app.js more generic and added prayers.json

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var PushBullet = require('pushbullet');
 var config = require('./config');
-var questions = require('./questions');
 var random = require('random-js')();
 
+var data =require('./' + process.argv[2]) 
+
 var pusher = new PushBullet(config.pushbullet.apiKey);
-pusher.note(config.pushbullet.deviceId, getRandomQuestion(), '', function(err, response) {
+pusher.note(config.pushbullet.deviceId, getRandom(), '', function(err, response) {
   if(err) {
     return console.log(err);
   }
@@ -12,7 +13,7 @@ pusher.note(config.pushbullet.deviceId, getRandomQuestion(), '', function(err, r
   console.log(response.title);
 });
 
-function getRandomQuestion() {
-  var value = random.integer(0, questions.length - 1);
-  return questions[value];
+function getRandom() {
+  var value = random.integer(0, data.length - 1);
+  return data[value];
 }
